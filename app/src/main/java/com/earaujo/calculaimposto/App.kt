cbcc96829911fa6b31810c65ab7705a38a88ed03
Eduardo Nunes <earaujo@gmail.com>
2019-06-18 20:05:07 -0300

    Increase jacoco version and refactored the application

--- app/src/main/java/com/earaujo/calculaimposto/App.kt
+++ app/src/main/java/com/earaujo/calculaimposto/App.kt
@@ -4,12 +4,5 @@ import android.app.Application
 
 open class App: Application() {
 
-    private lateinit var calculaImposto: CalculaImposto
-
-    override fun onCreate() {
-        super.onCreate()
-        calculaImposto = CalculaImposto(Federal(), Municipal())
-    }
-
     open fun getCalculaImposto() = CalculaImposto(Federal(), Municipal())
 }
